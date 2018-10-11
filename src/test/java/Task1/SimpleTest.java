a0b9b6d34f39acd9b892623821424d1666d07cdf
Maksim_Cha <LuckyRogerStar@yandex.ru>
2018-10-11 19:14:31 +0300

    close browser after testing

--- src/test/java/Task1/SimpleTest.java
+++ src/test/java/Task1/SimpleTest.java
@@ -4,11 +4,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import static java.lang.System.setProperty;
+import static org.testng.Assert.assertEquals;
+
 public class SimpleTest {
 
     @Test
     public void simpleTest(){
+        setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        assertEquals(driver.getTitle(), "Home Page");
+
+        driver.close();
+        //driver.quit();
     }
 }
