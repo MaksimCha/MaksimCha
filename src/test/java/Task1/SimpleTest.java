30450da70baf17725e155b6c2e554c8bae697804
Maksim_Cha <LuckyRogerStar@yandex.ru>
2018-10-11 19:36:51 +0300

    Login on site

--- src/test/java/Task1/SimpleTest.java
+++ src/test/java/Task1/SimpleTest.java
@@ -1,8 +1,11 @@
 package Task1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.System.setProperty;
 import static org.testng.Assert.assertEquals;
@@ -13,11 +16,28 @@ public class SimpleTest {
     public void simpleTest(){
         setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
 
+        //Open BR
         WebDriver driver = new ChromeDriver();
+
+        //Manage window
+        driver.manage().window().maximize();
+        driver.manage().window().fullscreen();
+
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        //Navigate
         driver.navigate().to("https://epam.github.io/JDI/index.html");
 
+        //Assert
         assertEquals(driver.getTitle(), "Home Page");
 
+        //Login
+        driver.findElement(By.cssSelector(".profile-photo")).click();
+        driver.findElement(By.cssSelector("[id = 'Name']")).sendKeys("epam");
+        driver.findElement(By.cssSelector("[id = 'Password']")).sendKeys("1234");
+        driver.findElement(By.cssSelector(".login [type = 'submit']")).click();
+
+        //Close browser
         driver.close();
         //driver.quit();
     }
