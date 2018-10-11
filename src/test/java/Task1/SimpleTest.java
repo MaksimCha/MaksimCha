2bf3ba4bc97b83aa9c2bf3ddc71f7a5d0f60c019
Maksim_Cha <LuckyRogerStar@yandex.ru>
2018-10-11 19:01:23 +0300

    add src

---
+++ src/test/java/Task1/SimpleTest.java
@@ -0,0 +1,14 @@
+package Task1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void simpleTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+    }
+}
