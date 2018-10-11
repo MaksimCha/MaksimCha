75f568f49278335c45af7a53558fe8b3aafd3311
Maksim_Cha <LuckyRogerStar@yandex.ru>
2018-10-11 19:49:07 +0300

    add assertion to equals header

--- src/test/java/Task1/SimpleTest.java
+++ src/test/java/Task1/SimpleTest.java
@@ -2,6 +2,7 @@ package Task1;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -36,6 +37,9 @@ public class SimpleTest {
         driver.findElement(By.cssSelector("[id = 'Name']")).sendKeys("epam");
         driver.findElement(By.cssSelector("[id = 'Password']")).sendKeys("1234");
         driver.findElement(By.cssSelector(".login [type = 'submit']")).click();
+
+        WebElement mainTitle = driver.findElement(By.cssSelector("h3.main-title"));
+        assertEquals(mainTitle.getText(), "EPAM FRAMEWORK WISHES…");
 
         //Close browser
         driver.close();
