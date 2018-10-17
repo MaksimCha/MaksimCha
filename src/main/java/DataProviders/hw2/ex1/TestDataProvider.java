c2ec4af4c424c013c58e865bc807b074cec71c39
Maksim_Cha <LuckyRogerStar@yandex.ru>
2018-10-17 11:35:51 +0300

    homework 2, exercise 1 done

---
+++ src/main/java/DataProviders/hw2/ex1/TestDataProvider.java
@@ -0,0 +1,22 @@
+package DataProviders.hw2.ex1;
+
+import org.testng.annotations.DataProvider;
+
+public class TestDataProvider {
+
+    @DataProvider
+    public Object[][] testDataProvider() {
+        return new Object[][] {
+                {"To include good practices\n" +
+                 "and ideas from successful\n" +
+                 "EPAM project", 0},
+                {"To be flexible and\n" +
+                 "customizable", 1},
+                {"To be multiplatform", 2},
+                {"Already have good base\n" +
+                 "(about 20 internal and\n" +
+                 "some external projects),\n" +
+                 "wish to get more…", 3}
+        };
+    }
+}
---
+++ src/test/java/hw2/ex1/parallelDataProviderImageTesting.java
@@ -0,0 +1,34 @@
+package hw2.ex1;
+
+import DataProviders.hw2.ex1.TestDataProvider;
+import base.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
+public class parallelDataProviderImageTesting extends TestBase {
+
+    @Test(dataProvider = "testDataProvider", dataProviderClass = TestDataProvider.class, threadPoolSize = 4)
+    public void textImageTesting(String text, int i){
+
+        //1. Open BR
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2. Navigate
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        //3. Assert 4 texts below 4 pictures on the Index Page
+        List<WebElement> testedText = driver.findElements(By.cssSelector("div.benefit > span"));
+        assertEquals(testedText.get(i).getText(), text);
+
+        //4. Close driver
+        driver.close();
+    }
+}
