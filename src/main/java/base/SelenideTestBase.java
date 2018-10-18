7a63fbb0451bdaf7d003b16c03d0a87014499651
Maksim_Cha <LuckyRogerStar@yandex.ru>
2018-10-18 23:13:54 +0300

    hometask 4 begin done

--- src/main/java/base/SelenideTestBase.java
+++ src/main/java/base/SelenideTestBase.java
@@ -3,11 +3,17 @@ package base;
 import com.codeborne.selenide.Configuration;
 import org.testng.annotations.BeforeSuite;
 
+import static java.lang.System.setProperty;
+
 public class SelenideTestBase {
 
     @BeforeSuite
     public void beforeSuite(){
         Configuration.browser = "chrome";
+        Configuration.screenshots = false;
+        Configuration.timeout = 10000;
+        Configuration.pollingInterval = 300;
+        setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
     }
 
 }
---
+++ src/test/java/hw4/SelenidePageObjectTest.java
@@ -0,0 +1,47 @@
+package hw4;
+
+import base.SelenideTestBase;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pageObject.TestedPageSelenide;
+
+import static com.codeborne.selenide.Selenide.page;
+import static com.codeborne.selenide.Selenide.sleep;
+import static enums.Titles.HOME_PAGE_TITLE;
+import static enums.Titles.LOGIN_TITLE;
+import static enums.Users.PITER_CHAILOWSKI;
+
+public class SelenidePageObjectTest extends SelenideTestBase {
+
+    private TestedPageSelenide testedPageSelenide;
+
+    @BeforeClass
+    public void beforeClass() {
+        testedPageSelenide = page(TestedPageSelenide.class);
+    }
+
+    @Test
+    public void simpleTest() {
+
+        //1. Open test site by URL
+        testedPageSelenide.openPage();
+
+        //2. Assert Browser title
+        testedPageSelenide.checkTitle(HOME_PAGE_TITLE.title);
+
+        //3. Perform login
+        testedPageSelenide.login(PITER_CHAILOWSKI.login, PITER_CHAILOWSKI.password);
+
+        //4. Assert User name in the left-top side of screen that user is loggined
+        testedPageSelenide.checkLoginTitle(LOGIN_TITLE.title);
+
+        //5. Open through the header menu Service -> Dates Page
+        testedPageSelenide.realiseDatesButton();
+        testedPageSelenide.checkDatesPageDisplaying();
+
+        //6. Using drag-and-drop set Range sliders.
+        // left sliders - the most left position,
+        // right slider - the most rigth position
+
+    }
+}
