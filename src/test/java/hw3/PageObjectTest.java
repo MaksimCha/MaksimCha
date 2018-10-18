23184d75678b5c77d3f601c14fdd37822b355ec4
Maksim_Cha <LuckyRogerStar@yandex.ru>
2018-10-18 20:30:11 +0300

    add enums to third homework

--- src/test/java/hw3/PageObjectTest.java
+++ src/test/java/hw3/PageObjectTest.java
@@ -13,36 +13,13 @@ import pageObject.TestedPage;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
+import static enums.Titles.*;
 import static enums.Users.PITER_CHAILOWSKI;
 
 public class PageObjectTest extends TestBase {
 
     private WebDriver driver;
     private TestedPage testedPage;
-
-    //TODO enum
-    private final String MAIN_HEADER_TITLE = "EPAM FRAMEWORK WISHES…";
-    private final String MAIN_HEADER_TEXT =
-            "LOREM IPSUM DOLOR SIT AMET, " +
-                    "CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT " +
-                    "LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD " +
-                    "EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT " +
-                    "DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM " +
-                    "DOLORE EU FUGIAT NULLA PARIATUR.";
-    private final String LOGIN_TITLE = "PITER CHAILOVSKII";
-    private final String FIRST_IMAGE_TITLE =
-            "To include good practices\n" +
-                    "and ideas from successful\n" +
-                    "EPAM project";
-    private final String SECOND_IMAGE_TITLE =
-            "To be flexible and\n" +
-                    "customizable";
-    private final String THIRD_IMAGE_TITLE = "To be multiplatform";
-    private final String FOURTH_IMAGE_TITLE =
-            "Already have good base\n" +
-                    "(about 20 internal and\n" +
-                    "some external projects),\n" +
-                    "wish to get more…";
 
     @BeforeClass
     public void beforeClass() {
@@ -68,23 +45,23 @@ public class PageObjectTest extends TestBase {
         testedPage.open(driver);
 
         //2. Assert
-        testedPage.checkTitle(driver, "Home Page");
+        testedPage.checkTitle(driver, HOME_PAGE_TITLE.title);
 
         //3. Login
         testedPage.login(PITER_CHAILOWSKI.login, PITER_CHAILOWSKI.password);
 
         //4. Assert User name in the left-top side of screen that user is loggined
-        testedPage.checkLoginTitle(LOGIN_TITLE);
+        testedPage.checkLoginTitle(LOGIN_TITLE.title);
 
         //5. Assert Browser title
-        testedPage.checkTitle(driver, "Home Page");
+        testedPage.checkTitle(driver, HOME_PAGE_TITLE.title);
 
         //6. Assert that there are 4 items on the header section are displayed and they have proper texts
         ArrayList<String> expectedTitles = new ArrayList<String>();
-        expectedTitles.add("HOME");
-        expectedTitles.add("CONTACT FORM");
-        expectedTitles.add("SERVICE");
-        expectedTitles.add("METALS & COLORS");
+        expectedTitles.add(FIRST_HEADER_ITEM_TITLE.title);
+        expectedTitles.add(SECOND_HEADER_ITEM_TITLE.title);
+        expectedTitles.add(THIRD_HEADER_ITEM_TITLE.title);
+        expectedTitles.add(FOURTH_HEADER_ITEM_TITLE.title);
 
         testedPage.checkHeaderItemText(expectedTitles);
 
@@ -93,16 +70,16 @@ public class PageObjectTest extends TestBase {
 
         //8. Assert that there are 4 texts on the Index Page under icons and they have proper text
         ArrayList<String> expectedImageTitles = new ArrayList<String>();
-        expectedImageTitles.add(FIRST_IMAGE_TITLE);
-        expectedImageTitles.add(SECOND_IMAGE_TITLE);
-        expectedImageTitles.add(THIRD_IMAGE_TITLE);
-        expectedImageTitles.add(FOURTH_IMAGE_TITLE);
+        expectedImageTitles.add(FIRST_IMAGE_TITLE.title);
+        expectedImageTitles.add(SECOND_IMAGE_TITLE.title);
+        expectedImageTitles.add(THIRD_IMAGE_TITLE.title);
+        expectedImageTitles.add(FOURTH_IMAGE_TITLE.title);
 
         testedPage.checkImageTitles(expectedImageTitles);
 
         //9. Assert a text of the main header
-        testedPage.checkMainHeaderText(MAIN_HEADER_TITLE,
-                MAIN_HEADER_TEXT);
+        testedPage.checkMainHeaderText(MAIN_HEADER_TITLE.title,
+                MAIN_HEADER_TEXT.title);
 
         //10. Assert that there is the iframe in the center of page
         testedPage.checkFrameDisplaying();
@@ -118,7 +95,7 @@ public class PageObjectTest extends TestBase {
         testedPage.checkSubHeaderDisplaying();
 
         //14. Assert that JDI GITHUB is a link and has a proper URL
-        testedPage.checkSubHeaderLink("https://github.com/epam/JDI");
+        testedPage.checkSubHeaderLink(SUBHEADER_LINK.title);
 
         //15. Assert that there is Left Section
         testedPage.checkLeftSectionDisplaying();
