6411b42ed37cec2c0e088c3dfe0c29bb94fe3dde
Maksim_Cha <LuckyRogerStar@yandex.ru>
2018-10-26 07:46:27 +0300

    6 hometask setters in class

--- src/main/java/dataTableRows/UserTableRow.java
+++ src/main/java/dataTableRows/UserTableRow.java
@@ -1,15 +1,11 @@
 package dataTableRows;
 
+import java.util.Map;
+
 public class UserTableRow {
     private Integer number;
     private String user;
     private String description ;
-
-    public UserTableRow(Integer number, String user, String description) {
-        this.number = number;
-        this.user = user;
-        this.description = description;
-    }
 
     public Integer getNumber() {
         return number;
@@ -21,5 +17,25 @@ public class UserTableRow {
 
     public String getDescription() {
         return description;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public static UserTableRow createRow(Map<String, String> entry) {
+        UserTableRow userRow = new UserTableRow();
+        userRow.setNumber(Integer.parseInt(entry.get("number")));
+        userRow.setUser(entry.get("user"));
+        userRow.setDescription(entry.get("description"));
+        return userRow;
     }
 }
