7881f8872e1e81f1009ed74659a341dbf7d6b9af
MaksimCha <LuckyRogerStar@yandex.ru>
2018-11-16 20:52:10 +0300

    4 hw fix

--- src/main/java/dataProviders/JDIDataProvider.java
+++ src/main/java/dataProviders/JDIDataProvider.java
@@ -1,18 +1,21 @@
 package dataProviders;
 
-import JSONReaders.JSONReader;
+import JSONReaders.CustomSerializer;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import entities.FormData;
 import org.testng.annotations.DataProvider;
 
+import java.io.FileReader;
+import java.lang.reflect.Type;
 import java.util.Map;
 
 public class JDIDataProvider {
 
     @DataProvider(parallel = true)
     public Object[][] JDIFormDataProvider() throws Exception {
-        // TODO This will be better with TypeToken
-        JSONReader reader = new JSONReader();
-        Map<String, FormData> dataSet = reader.readFromJSON();
+        Map<String, FormData> dataSet = readFromJSON();
         Object[][] objectDataSet = new Object[dataSet.size()][1];
         int i = 0;
         for (FormData data : dataSet.values()) {
@@ -20,5 +23,13 @@ public class JDIDataProvider {
             ++i;
         }
         return objectDataSet;
+    }
+
+    private Map<String, FormData> readFromJSON() throws Exception{
+        Type itemsMapType = new TypeToken<Map<String, FormData>>() {}.getType();
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(FormData.class, new CustomSerializer());
+        Gson gson = builder.create();
+        return gson.fromJson(new FileReader("src\\main\\resources\\JDI_ex8_metalsColorsDataSet.json"), itemsMapType);
     }
 }
